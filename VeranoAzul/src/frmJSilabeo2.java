247d28959d444cf72c59b8925d18c3e311ed0de4
culexjj <culex.jj@gmail.com>
2022-10-01 18:01:46 +0200

    Linea base: 0.1.2 (01/10/2022)

--- VeranoAzul/src/frmJSilabeo2.java
+++ VeranoAzul/src/frmJSilabeo2.java
@@ -1,7 +1,7 @@
 /**
  * @author edmond duke
  * @author MODIFICADO culex.jj
- * @version  0.1.1 (10/08/2022)
+ * @version  0.1.2 (01/10/2022)
  */
 import java.awt.Font;
 import java.awt.GridLayout;
